cfa23b1e62178996eb06ae708920b27c8bbc2159
Stephen Jayakar <aj12ay@gmail.com>
2026-06-12 17:57:46 -0700

    Match Emacs bind-key prompt

--- install.ts
+++ install.ts
@@ -103,7 +103,7 @@ function installPersonalCommands(editor: Editor): void {
   const bindKey = async ({ editor, args }: CommandContext) => {
     const sequence = args[0] ?? await editor.readKeySequence("Press key sequence to bind: ")
     if (!sequence) return
-    const command = args[1] ?? await editor.completingRead(`Command to bind to ${sequence}: `, {
+    const command = args[1] ?? await editor.completingRead(`Command to bind to '${sequence}': `, {
       collection: editor.commands.names(),
       history: "command",
     })
